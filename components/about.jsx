3332c9b7acdaeafcf964e1b67013cae3c40fca63
evan <evanm689@gmail.com>
2022-06-23 13:12:21 -0700

    add portfolio section

--- components/about.jsx
+++ components/about.jsx
@@ -5,14 +5,21 @@ export default function About() {
 
         <div className={styles.container}>
             <div className={styles.title} >
-                <h2>Experience, Skills, and Education</h2>
+                
             </div>
             <div className={styles.content}>
+                <h2>About Me</h2>
+                <p>I'm baby celiac chartreuse lo-fi, subway tile normcore vice cold-pressed woke selvage godard.
+                    +1 asymmetrical williamsburg, thundercats succulents hot chicken wolf green juice woke coloring
+                    book four dollar toast beard aesthetic. Pinterest iceland poke glossier, meditation copper mug biodiesel kickstarter leggings.
+                    Chicharrones scenester lumbersexual irony blog wolf. Lumbersexual butcher fam blog microdosing pok pok.
+                    Everyday carry mixtape thundercats, celiac lumbersexual austin irony.</p>
+                <h2>Skills</h2>
                 <ul>
                     <li>Proficient in Javascript and associated technologies such as React, Next, Angular, Redux, Node, Express </li>
                     <li>Proficient in SQl and NOSQL database technologies and consuming REST and SOAP API's </li>
                     <li>Proficient in OOP principals and Java </li>
-                    <li>Experienced with Git, Python, XML, C#, Linux, Bash, CSS, HTML  </li>
+                    <li>Experienced with Git, Python, XML, C#, Linux, Bash, CSS, HTML </li>
                     <li>BS in Computer Science from University of Maryland December 2021 </li>
                 </ul>
             </div>
--- components/portfolio.jsx
+++ components/portfolio.jsx
@@ -1,10 +1,61 @@
 import styles from './portfolio.module.scss'
+import Image from 'next/image';
+import ImageGallery from 'react-image-gallery';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function Portfolio() {
+
+    const storefrontImages = [
+        {
+            original: '/images/storefront/storefront1.png',
+        },
+        {
+            original: '/images/storefront/storefront2.png',
+        },
+    ];
+
+    const recipeboxImages = [
+        {
+            original: '/images/recipebox/recipebox1.png',
+        },
+        {
+            original: '/images/recipebox/recipebox2.png',
+        },
+    ];
+
     return (
 
         <div className={styles.container}>
-            <h2>Portfolio</h2>
+
+            <h1>Portfolio</h1>
+            <div className={styles.projectContainer}>
+                <div className={styles.textContainer}>
+                    <h3>Storefront</h3>
+                    <p>A personal recipe organizer for the home cook. Designed to be navigable and readable on all screen sizes.
+                        Implemented user interface using React with MaterialUI and custom CSS for styling.Focus on implementing intuitive interface.
+                        Created API using Express and Mongoose with MongoDB for storage.Supports all CRUD operations.</p>
+                    <button className={styles.button}>Check It Out Live </button>
+
+                </div>
+                <div className={styles.galleryContainer}>
+                    <h3>Storefront Gallery</h3>
+                    <ImageGallery items={storefrontImages} showPlayButton={false} showBullets={true} showThumbnails={false} showFullscreenButton={false} />
+                </div>
+            </div>
+            <div className={styles.projectContainerReverse}>
+                <div className={styles.textContainer}>
+                    <h3>RecipeBox</h3>
+                    <p>A personal recipe organizer for the home cook. Designed to be navigable and readable on all screen sizes.
+                        Implemented user interface using React with MaterialUI and custom CSS for styling.Focus on implementing intuitive interface.
+                        Created API using Express and Mongoose with MongoDB for storage.Supports all CRUD operations.</p>
+                    <button className={styles.button}>Check It Out Live </button>
+                </div>
+                <div className={styles.galleryContainer}>
+                    <h3>RecipeBox Gallery</h3>
+                    <ImageGallery items={recipeboxImages} showPlayButton={false} showBullets={true} showThumbnails={false} showFullscreenButton={false} />
+                </div>
+            </div>
 
 
         </div>
